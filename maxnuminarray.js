d8e732fe5776f33e06234659d10baade0bf0ad98
Pankaj-Gurung <grgpankaj004@gmail.com>
2024-08-15 14:40:16 +0545

    comments added

--- maxnuminarray.js
+++ maxnuminarray.js
@@ -26,3 +26,8 @@ const maxNumber = () => {
 
 maxNumber(array)
 
+// explaination: The max number initially is the first value in the array and it needs to kept somewhere so, we declare maxNum. We use for loop
+// which puts the first number here which is 1 initially and then it checks the lenght, as the lenght is smallar than 5, it continues, then it 
+// compares 1 and 2, since 2 is greater than 1, the value in maxNum is updated to 2 and the i now becomes 2, now in the 2 place of array, there is
+// number 3, now before checking it checks the lenght, the lenght is still smaller than 5 and so this continues.
+
